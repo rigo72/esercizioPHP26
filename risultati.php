dfb0ad12d47405ebc19f165ca0961f4a0a7a16b6
rigo72 <mattia.gori27@gmail.com>
2024-10-24 10:45:51 +0200

    Commit 2

--- risultati.php
+++ risultati.php
@@ -6,10 +6,47 @@
     <title>Document</title>
 </head>
 <body>
+    <table border = "1">
+        <tr>
+            <td>Nome</td>
+            <td>Cognome</td>
+            <td>Data di nascita</td>
+            <td>Orario di rientro a casa</td>
+            <td>Metodo per andare a scuola</td>
+            <td>Corsi Pomeridiani</td>
+        </tr>
     <?php
-        $nome
-        $cognome 
-        $
-    ?>
+        $nome = $_GET["nome"];
+        $cognome = $_GET["cognome"];
+        $dataDiNascita = $_GET["dataNascita"];
+        $rientroACasa = $_GET["orarioRitorno"];
+        $mezzoUtilizzatoPerAndareAScuola = $_GET["metodoPerAndareAScuola"];
+        $corsoPomeridiano = array();
+        if(isset($_GET["corsoPomeridiano1"])){
+            array_push($corsoPomeridiano, $_GET["corsoPomeridiano1"]);
+        }
+        if(isset($_GET["corsoPomeridiano2"])){
+            array_push($corsoPomeridiano , $_GET["corsoPomeridiano2"]);
+        }
+        if(isset($_GET["corsoPomeridiano3"])){
+            array_push($corsoPomeridiano , $_GET["corsoPomeridiano3"]);
+        }
+        if($corsoPomeridiano == null){
+            $corsoPomeridiano = "Nessun Corso";
+        }
+        echo 
+        "<tr> 
+            <td>{$nome}</td> 
+            <td>{$cognome}</td> 
+            <td>{$dataDiNascita}</td>
+            <td>{$rientroACasa}</td> 
+            <td>{$mezzoUtilizzatoPerAndareAScuola}</td> <td>";
+        foreach($corsoPomeridiano as $value){
+           echo "{$value} <br>";
+        }
+    ?>  
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
